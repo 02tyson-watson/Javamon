153d20a3bf9a5f4ff4f258ab373fececf3d80dd0
Tyson <02tyson.watson@gulllakecs.org>
2019-12-06 10:01:38 -0500

    Imported Random and Started on Main Menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
+import java.util.Random;
 import java.util.Scanner;
 class Main {
     // public static void
     public static void main(String[] args) {
+        Random rn = new Random();
         Pokemon bulbasaur = new Pokemon();
         bulbasaur.name = "Bulbasaur";
         bulbasaur.level = 1;
@@ -17,7 +19,7 @@ class Main {
         charmander.health = 20 * (charmander.level/2);
         Attack scrape = new Attack();
         scrape.name = "Scrape";
-        scrape.dmg = 2;
+        scrape.dmg = rn.nextInt(6);
         String pokeslot1 = "none";
         String pokeslot2 = "none";
         String pokeslot3 = "none";
@@ -53,27 +55,38 @@ class Main {
             case "Charmander":
                 System.out.println("You chose Charmander!");
                 break;
+                pokeslot1 = "Bulbasuar";
             case "charmander":
                 System.out.println("You chose Charmander!");
                 break;
+                pokeslot1 = "Bulbasuar";
             case "Squirtle":
                 System.out.println("You chose Squirtle!");
                 break;
+                pokeslot1 = "Bulbasuar";
             case "squirtle":
                 System.out.println("You chose Squirtle!");
                 break;
+                pokeslot1 = "Bulbasuar";
             case "Bulbasuar":
                 System.out.println("You chose Bulbasuar!");
                 break;
+                pokeslot1 = "Bulbasuar";
             case "bulbasuar":
                 System.out.println("You chose Bulbasuar!");
                 break;
+                pokeslot1 = "Bulbasuar";
             default:
                 System.out.println("Thats not one of the options. \n");
         }
+        while (true){
+            System.out.println("What do you want to do? \n \n Walk \n Bag \n Pokemon \n Pokedex \n ");
+            String command = sc.nextLine();
+            switch (command){
+                case "Walk":
+                    System.out.println("You walked forward. \n");
+            }
 
-
-
-
+        }
     }
 }
